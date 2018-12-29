8bf3bf9ad974435427d0bda793bf32d69290e64c
yd <892195000@qq.com>
2018-12-29 17:36:12 +0800

    Initial commit

---
+++ registry/src/main/java/com/yd1994/springcloudcli/registry/RegistryApplication.java
@@ -0,0 +1,14 @@
+package com.yd1994.springcloudcli.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
+
