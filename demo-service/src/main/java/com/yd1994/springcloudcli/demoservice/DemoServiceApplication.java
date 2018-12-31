c3f30233b1d0611d7f21f5c317fba64efe9a9a10
yd <892195000@qq.com>
2018-12-31 21:19:16 +0800

    针对config-server修改

--- demo-service/src/main/java/com/yd1994/springcloudcli/demoservice/DemoServiceApplication.java
+++ demo-service/src/main/java/com/yd1994/springcloudcli/demoservice/DemoServiceApplication.java
@@ -2,6 +2,7 @@ package com.yd1994.springcloudcli.demoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
--- demo-service/src/main/java/com/yd1994/springcloudcli/demoservice/controller/TestController.java
+++ demo-service/src/main/java/com/yd1994/springcloudcli/demoservice/controller/TestController.java
@@ -2,6 +2,8 @@ package com.yd1994.springcloudcli.demoservice.controller;
 
 import com.yd1994.springcloudcli.demoservice.client.UserInfoClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,15 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @RequestMapping("/test")
 public class TestController {
 
     @Autowired
     private UserInfoClient userInfoClient;
 
+    @Value("${testconfig}")
+    private String configMsg;
+
     @GetMapping("/user_info/{name}")
     public ResponseEntity getUserInfo(@PathVariable("name") String name) {
         return ResponseEntity.ok(this.userInfoClient.getUserInfoByName(name));
+    }
+
+    @GetMapping("/testconfig")
+    public ResponseEntity getConfig() {
+        return ResponseEntity.ok(this.configMsg);
     }
 
 }
